360462431a90a9c2f31d3e1e53b9a41bf5cd8859
mitsunao.miyamoto <mitu3270uw@yahoo.co.jp>
2019-07-19 05:18:03 +0000

    jQuery version change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
         @include('commons.footer')
     @endif
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
        
